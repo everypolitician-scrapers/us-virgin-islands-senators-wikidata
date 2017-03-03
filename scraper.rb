6cd1406d4a19decbf2bb510b3336ffba2a10e5d9
Tony Bowden <tony@tmtm.com>
2017-03-03 23:19:34 +0000

    Also fetch Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,14 @@
 
 require 'wikidata/fetcher'
 
+query = <<-EOQ
+  SELECT DISTINCT ?item {
+    ?item wdt:P39 wd:Q20846080 .
+  }
+EOQ
+ids = EveryPolitician::Wikidata.sparql(query)
+
 en = WikiData::Category.new('Category:Senators of the Legislature of the United States Virgin Islands', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en })
 
